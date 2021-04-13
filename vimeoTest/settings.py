7d014870d139165404f866b4648fada0f1d5c3fe
Ahostility <ale1431999@gmail.com>
2021-04-13 11:45:28 +0300

    Debug

--- vimeoTest/settings.py
+++ vimeoTest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ra)w1^jhwxmez+!l!^0-d1-c)-1cl$=szcaq(udu24jvh=bu+g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
